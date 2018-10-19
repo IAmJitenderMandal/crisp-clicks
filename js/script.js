f12d3eea0246c1db0080381c363f6fa188f8db0d
jitenderMandal <jitendermandal>
2018-10-19 16:54:27 +0530

    dropdown menu for small screen devices

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var burger_menu_icon = document.querySelector('.dropdown');
+var nav_element = document.querySelector("header#header div.nav-logo nav div");
+
+console.log(nav_element);
+
+burger_menu_icon.addEventListener("click", function (e) {
+        nav_element.classList.toggle("menu");
+        
+})
