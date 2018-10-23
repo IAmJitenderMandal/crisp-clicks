3a52aba4803a4ed0f5906063ddead78f6bcfa87d
jitender mandal <iamjitender>
2018-10-23 16:16:58 +0530

    mobile menu added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var nav_element = document.querySelector("header#header div.nav-logo nav div");
 console.log(nav_element);
 
 burger_menu_icon.addEventListener("click", function (e) {
+  
         nav_element.classList.toggle("menu");
-        
+
 })
